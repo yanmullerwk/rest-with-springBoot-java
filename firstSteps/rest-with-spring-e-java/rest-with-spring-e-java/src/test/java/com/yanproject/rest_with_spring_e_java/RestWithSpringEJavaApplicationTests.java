2d7e29ce1e1e946d45ec04f0ff6fe5f041bdfb24
yanmullerwk <yan.muller.contact@gmail.com>
2024-11-29 18:23:32 -0300

    code: hello word first step

---
+++ firstSteps/rest-with-spring-e-java/rest-with-spring-e-java/src/test/java/com/yanproject/rest_with_spring_e_java/RestWithSpringEJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanproject.rest_with_spring_e_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringEJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
